775d9083cc2f38713e6ef175c8277620022300fa
Behruamm <behram95623@gmail.com>
2024-10-15 16:27:10 +0100

    connect the google sheet and final modification

--- script.js
+++ script.js
@@ -24,3 +24,22 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+    var formData = new FormData(this);
+
+    fetch('https://script.google.com/macros/s/AKfycbzO6meBA6DnssAGiDzGON_X4N3UNXPsT77n00TcaLQsCeQxIr2spPwcigEnf3Y8DWV0-w/exec', { // Replace with your Google Apps Script URL
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(result => {
+        alert(result); // Show success message
+        document.getElementById('contactForm').reset(); // Reset form fields
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('There was an error submitting the form.');
+    });
+});
